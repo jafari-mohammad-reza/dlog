76f520976e446ef934d93c4a474cfe567d719d5a
mohammadrezajafari <mohammadrezajafari.dev@gmail.com>
2025-08-01 14:30:59 +0330

    feat: add tracker

--- main.go
+++ main.go
@@ -8,16 +8,11 @@ import (
 	"github.com/docker/docker/client"
 	"io"
 	"log"
-	"os"
 	"sync"
+	"time"
 )
 
-type LogRec struct {
-	Name string
-	Logs io.ReadCloser
-}
-
-func streamClog(wg *sync.WaitGroup, dc *client.Client, cn container.Summary) {
+func streamClog(ctx context.Context, wg *sync.WaitGroup, dc *client.Client, containerID, name string) {
 	defer wg.Done()
 
 	logOptions := container.LogsOptions{
@@ -27,41 +22,64 @@ func streamClog(wg *sync.WaitGroup, dc *client.Client, cn container.Summary) {
 		Follow:     true,
 	}
 
-	logReader, err := dc.ContainerLogs(context.Background(), cn.ID, logOptions)
-	name := cn.Names[0]
+	logReader, err := dc.ContainerLogs(ctx, containerID, logOptions)
 	if err != nil {
 		log.Printf("Failed to get logs for container %s: %v\n", name, err)
 		return
 	}
-	defer logReader.Close()
+	defer func(logReader io.ReadCloser) {
+		if err := logReader.Close(); err != nil {
+			log.Printf("Failed to close log reader for %s: %s", name, err.Error())
+		}
+	}(logReader)
 
 	scanner := bufio.NewScanner(logReader)
 	for scanner.Scan() {
-		fmt.Printf("[%s] %s\n", name, scanner.Text())
+		fmt.Printf("[%s] %s: %s\n", time.Now().Format(time.RFC3339Nano), name, scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Printf("Error reading logs for %s: %v\n", name, err)
 	}
 }
+
+func watchContainers(ctx context.Context, dc *client.Client) {
+	tracked := make(map[string]bool)
+	var mu sync.Mutex
+
+	for {
+		containers, err := dc.ContainerList(ctx, container.ListOptions{
+			All:    true,
+			Latest: true,
+		})
+		if err != nil {
+			log.Printf("Failed to list containers: %v\n", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		mu.Lock()
+		wg := sync.WaitGroup{}
+
+		for _, cn := range containers {
+			if !tracked[cn.ID] {
+				tracked[cn.ID] = true
+				wg.Add(1)
+				go streamClog(ctx, &wg, dc, cn.ID, cn.Names[0])
+			}
+		}
+		mu.Unlock()
+		fmt.Println("new containers tracked")
+		time.Sleep(5 * time.Second)
+	}
+}
+
 func main() {
-	dc, err := client.NewClientWithOpts()
+	dc, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		fmt.Println("failed to create client: ", err.Error())
-		os.Exit(1)
+		log.Fatalf("Failed to create Docker client: %v\n", err)
 	}
-	containers, err := dc.ContainerList(context.Background(), container.ListOptions{
-		All:    true,
-		Latest: true,
-	})
-	if err != nil {
-		fmt.Printf("failed to get contaienr list %s", err.Error())
-		os.Exit(1)
-	}
-	wg := sync.WaitGroup{}
-	for _, cn := range containers {
-		wg.Add(1)
-		go streamClog(&wg, dc, cn)
-	}
-	wg.Wait()
+
+	ctx := context.Background()
+	watchContainers(ctx, dc)
 }
