810130413d763c79be9d6594e2717c563b1af9d7
Mohammadreza Jafari <mohammadrezajafari.dev@gmail.com>
2025-08-02 17:14:57 +0330

    feat: add build script and test on ubuntu server 20.04

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Building with vendored dependencies..."
+
+# Vendor dependencies first
+go mod vendor
+
+# Build static binary
+CGO_ENABLED=0 \
+GOOS=linux \
+GOARCH=amd64 \
+go build \
+    -mod=vendor \
+    -a \
+    -ldflags="-s -w -extldflags '-static'" \
+    -trimpath \
+    -installsuffix cgo \
+    -o dlog
+
+# Clean up
+rm -rf vendor/
+
+echo "Built: $(du -h dlog | cut -f1)"
+ldd dlog 2>&1 || file dlog
+316673
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 build:
-	GOOS=linux GOARCH=amd64 go build -o dlog
+	GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -trimpath -o dlog
 run:
 	@air -c .air.toml
