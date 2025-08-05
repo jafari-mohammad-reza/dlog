a58e308d50a7b39868e376622bd88c9c454de63b
Mohammadreza Jafari <mohammadrezajafari.dev@gmail.com>
2025-08-05 10:33:54 +0330

    feat: test writing log in aggregator

--- internal/aggregator/aggregator_test.go
+++ internal/aggregator/aggregator_test.go
@@ -3,28 +3,54 @@ package aggregator
 import (
 	"context"
 	"dlog/internal/conf"
+	"fmt"
 	"os"
+	"path"
 	"testing"
+	"time"
 )
 
 func TestAggregator(t *testing.T) {
 	cfg := &conf.Config{
 		HealthCheckPort: 8080,
 	}
-	err := os.Mkdir("logs", 0644)
-	if err != nil && !os.IsExist(err) {
+	if err := os.Mkdir("logs", 0777); err != nil && !os.IsExist(err) {
 		t.Fatalf("failed to create logs directory: %v", err)
 	}
 	defer os.RemoveAll("logs")
+
 	trackedChan := make(chan conf.TrackedOption)
 	recordChan := make(chan conf.RecordLog)
 	ag := NewAggregatorService(cfg, trackedChan, recordChan)
-	ctx, cancel := context.WithCancel(context.Background())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	errChan := ag.Start(ctx)
-	for err := range errChan {
-		if err != nil {
-			t.Errorf("Aggregator encountered an error: %v", err)
+
+	done := make(chan struct{})
+	go func() {
+		recordChan <- conf.RecordLog{
+			ContainerName: "test-container",
+			Log:           "some test log",
 		}
+		time.Sleep(500 * time.Millisecond)
+
+		content, err := os.ReadFile(path.Join("logs", fmt.Sprintf("%s-test-container.log", time.Now().Format(time.DateOnly))))
+		if err != nil {
+			t.Errorf("failed to read log file: %v", err)
+		} else if string(content) == "" {
+			t.Error("log file is empty")
+		}
+		close(done)
+	}()
+
+	errChan := ag.Start(ctx)
+
+	select {
+	case <-done:
+
+	case err := <-errChan:
+		t.Fatalf("Aggregator encountered an error: %v", err)
+	case <-ctx.Done():
+		t.Fatal("test timed out")
 	}
 }
